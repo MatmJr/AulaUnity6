3f1869242d2e7869ec53f48d062053a3f1b56a50
MatmJr <matmj@cin.ufpe.br>
2025-04-29 08:54:28 -0300

    Reconectando e subindo para Aula2-PlayerInput

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb; 
+    [SerializeField] float speed;
+    [SerializeField] float jumpingPower;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+    private float horizontal;
+
+    #region MOVE_CONTROLLS
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+    #endregion
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(
+            groundCheck.position, 
+            new Vector2(0.8f,0.1f), 
+            CapsuleDirection2D.Horizontal, 
+            0, 
+            groundLayer
+        );
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# AulaUnity6
