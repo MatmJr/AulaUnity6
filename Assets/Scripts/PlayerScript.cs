539b5e5a37cda01cc8f49bf342f986cac36ae65a
MatmJr <matmj@cin.ufpe.br>
2025-05-02 17:15:52 -0300

    Aula 3 - Animações

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,17 +9,44 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform groundCheck;
     private float horizontal;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private int movingHash = Animator.StringToHash("Moving");
+    private int jumpingHash = Animator.StringToHash("Jumping");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     #region MOVE_CONTROLLS
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
     }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
     #endregion
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        animator.SetBool(movingHash, horizontal!=0);
+        animator.SetBool(jumpingHash, !IsGrounded());
+
+        if(horizontal > 0){
+            spriteRenderer.flipX = false;
+        }else {
+            spriteRenderer.flipX = true;
+        }
     }
     private bool IsGrounded()
     {
@@ -31,13 +58,4 @@ public class PlayerScript : MonoBehaviour
             groundLayer
         );
     }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if(context.performed && IsGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
-        }
-    }
-
 }
